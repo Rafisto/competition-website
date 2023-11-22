cf6487956c623a03bd0ca99227a526f9e73f3ce1
Grzegorz Jagielski <61905404+ModerNews@users.noreply.github.com>
2023-11-22 13:24:10 +0100

    Add mock data to database

---
+++ database/init/005-groups.sql
@@ -0,0 +1,10 @@
+insert into groups (id, name) values (1, 'Bison bison');
+insert into groups (id, name) values (2, 'Scolopax minor');
+insert into groups (id, name) values (3, 'Neophoca cinerea');
+insert into groups (id, name) values (4, 'Pitangus sulphuratus');
+insert into groups (id, name) values (5, 'Canis mesomelas');
+insert into groups (id, name) values (6, 'Scolopax major');
+insert into groups (id, name) values (7, 'Deroptyus accipitrinus');
+insert into groups (id, name) values (8, 'Felis caracal');
+insert into groups (id, name) values (9, 'Trachyphonus vaillantii');
+insert into groups (id, name) values (10, 'Tamandua tetradactyla');
