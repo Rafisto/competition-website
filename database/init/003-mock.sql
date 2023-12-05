cc85b226fad62d4a4d71e8d2518ca3c4b659dc73
Grzegorz Jagielski <61905404+ModerNews@users.noreply.github.com>
2023-12-05 18:25:11 +0100

    Fix database mock data
    Mock data was inserting ID instead of database generating it on its own. This led to serial indexing, and therefore PK constraint breaking

--- database/init/003-mock.sql
+++ database/init/003-mock.sql
@@ -1,20 +1,20 @@
-insert into contests (id, title, description, is_published) values (1, 'lorem ipsum dolor sit amet consectetuer adipiscing elit', 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.
+insert into contests (title, description, is_published) values ('lorem ipsum dolor sit amet consectetuer adipiscing elit', 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.
 
 Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', true);
-insert into contests (id, title, description, is_published) values (2, 'donec dapibus duis at velit eu est congue elementum', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', true);
-insert into contests (id, title, description, is_published) values (3, 'sapien cum sociis natoque penatibus et magnis dis parturient montes', 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
+insert into contests (title, description, is_published) values ('donec dapibus duis at velit eu est congue elementum', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', true);
+insert into contests (title, description, is_published) values ('sapien cum sociis natoque penatibus et magnis dis parturient montes', 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
 
 In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.', true);
-insert into contests (id, title, description, is_published) values (4, 'id ornare imperdiet sapien urna pretium nisl ut volutpat sapien', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', false);
-insert into contests (id, title, description, is_published) values (5, 'auctor gravida sem praesent id massa id', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
+insert into contests (title, description, is_published) values ('id ornare imperdiet sapien urna pretium nisl ut volutpat sapien', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', false);
+insert into contests (title, description, is_published) values ('auctor gravida sem praesent id massa id', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
 
 Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', true);
-insert into contests (id, title, description, is_published) values (6, 'odio in hac habitasse platea dictumst maecenas', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', true);
-insert into contests (id, title, description, is_published) values (7, 'quis turpis eget elit sodales scelerisque mauris', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.', true);
-insert into contests (id, title, description, is_published) values (8, 'in consequat ut nulla', 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.
+insert into contests (title, description, is_published) values ('odio in hac habitasse platea dictumst maecenas', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', true);
+insert into contests (title, description, is_published) values ('quis turpis eget elit sodales scelerisque mauris', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.', true);
+insert into contests (title, description, is_published) values ('in consequat ut nulla', 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.
 
 Phasellus in felis. Donec semper sapien a libero. Nam dui.', true);
-insert into contests (id, title, description, is_published) values (9, 'suscipit a feugiat et eros vestibulum ac est lacinia', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
+insert into contests (title, description, is_published) values ('suscipit a feugiat et eros vestibulum ac est lacinia', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
 
 Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', true);
-insert into contests (id, title, description, is_published) values (10, 'egestas metus aenean fermentum donec', 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.', false);
+insert into contests (title, description, is_published) values ('egestas metus aenean fermentum donec', 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.', false);
